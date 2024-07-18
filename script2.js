3c334b404cd470af5e2c50ee4c7b438b26eefcd9
Inbarasu <inbarasu.saravanan@signzy.com>
2024-07-18 12:36:50 +0530

    the timer and delete features are added and modified

--- script2.js
+++ script2.js
@@ -1,212 +1,257 @@
-// ****** DB CONNECTIVITY **** //
-// this variable is created to hold name of the db
-const taskDb = "taskManagementDb";
-// this variable will hold the db connectivity
-let taskDbConnect;
-// this line open / create if it does not exist the database with the specified name and the version
-const taskDbCreation = indexedDB.open(taskDb, 1);
-// this line opens the connections to the indexdb database named "taskManagementDb"the 1 is the verison number of the database if the database doesnot exit the ne the ongrandeneeded event will b e trigged anto allow for the schema changehandle the event where the database needs to be upgraded or created
-taskDbCreation.onupgradeneeded = (event) => {
-  // get the database connection from the event
-  taskDbConnect = event.target.result;
-  // if the object store (table) named task does not exist
-  if (!taskDbConnect.objectStoreNames.contains("task")) {
-    const objectStore = db.createObjectStore("tasks", { keyPath: "id", autoIncrement: true });
-    objectStore.createIndex("title", "title", { unique: false });
-  }
-};
-taskDbCreation.onsuccess = (event) => {
-  taskDbConnect = event.target.result;
-  console.log("DataBase opened successfully");
-};
-taskDbCreation.onerror = (event) => {
-  console.log("DataBase error:", event.target.errorCode);
-};
-
-// this peice of code will not allow you t o decrease the screen width
+// Check window size and redirect if width is too small
 function checkWindowSize() {
   if (window.innerWidth <= 750) {
-    // console.log("Window size is less than 750px")
     window.location.href = "warning.html";
   }
 }
 window.addEventListener("resize", checkWindowSize);
 window.addEventListener("load", checkWindowSize);
-// this peice of code is used  to add the new task in the to do list
+
+// DOM elements
 const addTaskBtn = document.querySelector("#addTask");
-// stroing the task object 
-const taskStorage = {}
-// This adds the new task to the todo container
-addTaskBtn.addEventListener("click", () => {
-  const newToDoChild = document.createElement("div");
-  // unique id for the task
-  function uniqueTaskId() {
-    const uniquetimestamp = Date.now().toString();
-    const randonTaskId = Math.floor(Math.random() * 10000).toString()
-    return (uniquetimestamp + randonTaskId).slice(-10);
-  };
-  const taskId = uniqueTaskId();
-  const taskIdContainer = [];
-  newToDoChild.setAttribute("id", taskId);
-  // Append the new element to the DOM before trying to retrieve it
-  const todoBox = document.getElementById("toDoBox");
-  todoBox.appendChild(newToDoChild);
-  console.log(newToDoChild); // This should now be part of the DOM
-  // Ensure that the ID is not already in the container
-  if (!taskIdContainer.includes(taskId)) {
-    const newTaskTitle = document.getElementById(taskId);
-    const taskTitleContent = document.getElementById("task-title-input");
-    const newTaskDescription = document.getElementById("task-description-input");
-    // console.log(taskTitleContent);
-    taskTitleContent.textContent = "Title";
-    newTaskDescription.textContent = "Start Typing...";
-    let dummytaskid = taskId;
-    taskTitleContent.addEventListener("blur", (event) => {
-      taskStorage[dummytaskid] = {
-        title: event.target.textContent
-      };
-      // console.log(taskStorage);
-    });
-    newTaskDescription.addEventListener("blur", (event) => {
-      taskStorage[dummytaskid].description = event.target.textContent;
-      // console.log(taskStorage);
-    });
+const taskBackdrop = document.getElementById("taskBackdrop");
+let desIdStore = [];
+
+// Unique ID generator
+function generateUniqueId() {
+  const timestamp = Date.now().toString();
+  const random = Math.floor(Math.random() * 1000000000).toString();
+  return (timestamp + random).slice(-10);
+}
+
+// Set the timer for a new task
+function setTimerNewTask(desId) {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  function getCurrentDayAndMonth() {
+    const now = new Date();
+    return {
+      day: daysOfWeek[now.getDay()],
+      month: `${monthsOfYear[now.getMonth()]} ${now.getFullYear()}`,
+      date: now.getDate()
+    };
   }
-  // const todoBox = document.getElementById("toDoBox");
-  const taskDescriptionModel = document.getElementById("taskDesModel");
-  // console.log("TDES TXT", taskDescriptionModel.value)
-  taskDescriptionModel.classList.remove("hidden");
-  // this function is used to copy the classname from add todo container to all other container
-  function copyOtherelementChild(source, target) {
-    target.className = source.className;
-    newToDoChild.classList.add("draggable-child");
-    newToDoChild.setAttribute("draggable", "true");
-    const taskBackdrop = document.getElementById("taskBackdrop");
-    taskBackdrop.classList.remove("hidden");
-    // to remove the pop description
-    taskBackdrop.addEventListener("click", () => {
-      taskDescriptionModel.classList.add("hidden");
-      taskBackdrop.classList.add("hidden");
-      const setTitleOfNewTask = document.getElementById(taskId)
-      // console.log(setTitleOfNewTask);
-      // console.log();
-    });
-    newToDoChild.classList.remove("justify-center");
-    newToDoChild.classList.remove("items-center");
-    newToDoChild.classList.add("p-6");
-    // if any new child is created if i click on it the ntask description mode l should pop up
-    newToDoChild.addEventListener("click", () => {
-      // console.log("the new task element is clicked")
-      const taskDescriptionModel = document.getElementById("taskDesModel")
-      const taskBackdrop = document.getElementById("taskBackdrop");
-      taskDescriptionModel.classList.remove("hidden");
-      taskBackdrop.classList.remove("hidden");
-      taskBackdrop.addEventListener("click", () => {
-        taskDescriptionModel.classList.add("hidden");
-        taskBackdrop.classList.add("hidden");
-      });
-      // changing the day and date content in the task description
-      const daysOfWeek = {
-        0: 'Sunday',
-        1: 'Monday',
-        2: 'Tuesday',
-        3: 'Wednesday',
-        4: 'Thursday',
-        5: 'Friday',
-        6: 'Saturday'
-      };
-      const monthsOfYear = {
-        0: 'January',
-        1: 'February',
-        2: 'March',
-        3: 'April',
-        4: 'May',
-        5: 'June',
-        6: 'July',
-        7: 'August',
-        8: 'September',
-        9: 'October',
-        10: 'November',
-        11: 'December'
-      };
-      // Function to get the current day and month in words
-      function getCurrentDayAndMonth() {
-        const now = new Date();
-        const dayOfWeek = daysOfWeek[now.getDay()];
-        const monthOfYear = monthsOfYear[now.getMonth()];
-        return {
-          day: dayOfWeek,
-          month: monthOfYear + " " + now.getFullYear(),
-          date: "0" + now.getDate()
-        };
-      };
-      // days manipulation for the task description model
-      const taskDate = document.getElementById("taskDate");
-      taskDate.textContent = getCurrentDayAndMonth().date;
-      const taskDay = document.getElementById("taskDay");
-      taskDay.textContent = getCurrentDayAndMonth().day;
-      const taskMonth = document.getElementById("taskMonth");
-      taskMonth.textContent = getCurrentDayAndMonth().month;
-      // This peice of code will give pop up to set the timer
-      const tasktimerIcon = document.getElementById("taskTimerImage");
-      tasktimerIcon.addEventListener("mouseenter", () => {
-        // console.log("mouse in")
-        const setTaskTimer = document.getElementById("setTaskTimer");
-        setTaskTimer.classList.remove("hidden");
-      });
-      setTaskTimer.addEventListener("mouseleave", () => {
-        setTaskTimer.classList.add("hidden");
-      });
-    });
-  };
-  copyOtherelementChild(addTaskBtn, newToDoChild);
-  // todoBox.insertBefore(newToDoChild, addTaskBtn)
-  console.log("Final storage object", taskStorage);
-  // This peice of code will enable the tasks to drag and drop as per the status
+
+  const currentDayAndMonth = getCurrentDayAndMonth();
+  document.getElementById("taskDate" + desId).textContent = currentDayAndMonth.date;
+  document.getElementById("taskDay" + desId).textContent = currentDayAndMonth.day;
+  document.getElementById("taskMonth" + desId).textContent = currentDayAndMonth.month;
+
+}
+
+// Copy attributes and properties from one element to another
+function copyOtherelementChild(source, target, id = 0) {
+  target.className = source.className;
+  target.classList.add("draggable-child", "p-6");
+  target.classList.remove("justify-center", "items-center");
+  target.setAttribute("draggable", "true");
+  const cloneDiv = source.cloneNode(true);
+  cloneDiv.setAttribute("id", id);
+  return cloneDiv;
+}
+
+// Initialize drag and drop functionality
+function initializeDragAndDrop() {
   const dragTaskContainer = document.querySelectorAll(".task-container");
   const dragtaskChild = document.querySelectorAll(".draggable-child");
-  // To pop up the task description
-  // darg the elements eas per the events
+
   dragtaskChild.forEach((dragIndTask) => {
-    dragIndTask.addEventListener("dragstart", (e) => {
-      dragIndTask.classList.add("dragging");
-      dragIndTask.classList.add("border-2");
-      dragIndTask.classList.add("border-yellow-300");
-    });
-    dragIndTask.addEventListener("dragend", () => {
-      dragIndTask.classList.remove("dragging");
-      dragIndTask.classList.remove("border-2");
-      dragIndTask.classList.remove("border-yellow-300");
-    });
+    if (!dragIndTask.dataset.dragInitialized) {
+      dragIndTask.dataset.dragInitialized = true;
+      dragIndTask.addEventListener("dragstart", () => {
+        dragIndTask.classList.add("dragging", "border-2", "border-yellow-300");
+      });
+
+      dragIndTask.addEventListener("dragend", () => {
+        dragIndTask.classList.remove("dragging", "border-2", "border-yellow-300");
+      });
+    }
   });
+
   dragTaskContainer.forEach((dropingTaskStatus) => {
-    dropingTaskStatus.addEventListener("dragover", (e) => {
-      // dropingTaskStatus.classList.add("border");
-      e.preventDefault();
-    });
-    dropingTaskStatus.addEventListener("dragenter", (e) => {
-      dropingTaskStatus.classList.add("border-2");
-      dropingTaskStatus.classList.add("border-green-500");
-      e.preventDefault();
-    });
-    dropingTaskStatus.addEventListener("dragleave", (e) => {
-      dropingTaskStatus.classList.remove("border-2");
-      e.preventDefault();
-    });
-    dropingTaskStatus.addEventListener("drop", (e) => {
-      e.preventDefault();
-      dropingTaskStatus.classList.remove("border-2");
-      const draggingElement = document.querySelector(".dragging")
-      dropingTaskStatus.appendChild(draggingElement);
-    });
+    if (!dropingTaskStatus.dataset.dropInitialized) {
+      dropingTaskStatus.dataset.dropInitialized = true;
+      dropingTaskStatus.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+
+      dropingTaskStatus.addEventListener("dragenter", (e) => {
+        dropingTaskStatus.classList.add("border-2", "border-green-500");
+        e.preventDefault();
+      });
+
+      dropingTaskStatus.addEventListener("dragleave", (e) => {
+        dropingTaskStatus.classList.remove("border-2");
+        e.preventDefault();
+      });
+
+      dropingTaskStatus.addEventListener("drop", (e) => {
+        e.preventDefault();
+        dropingTaskStatus.classList.remove("border-2");
+        const draggingElement = document.querySelector(".dragging");
+        dropingTaskStatus.appendChild(draggingElement);
+      });
+    }
   });
-});
-// to pop up the menu bar in the to do page
+}
+
+// deleting element from the array 
+function delDesId(id) {
+  desIdStore = desIdStore.filter(item => item !== id);
+}
+// set timer for the task
+function setTimer(id) {
+  const fromHour = document.getElementById("fromHour" + id);
+  const fromMin = document.getElementById("fromMin" + id);
+  const toHour = document.getElementById("toHour" + id);
+  const toMin = document.getElementById("toMin" + id);
+  const dateAndTime = new Date();
+  fromHour.textContent = dateAndTime.getHours();
+  fromMin.textContent = dateAndTime.getMinutes();
+}
+
+// Handle task creation
+function createTask() {
+  const newToDoChild = document.createElement("div");
+  const todoBox = document.getElementById("toDoBox");
+  const desUniqueId = generateUniqueId();
+  const desTemplate = document.getElementById("descriptionModelTemplate").content.cloneNode(true);
+  const rootElement = desTemplate.getElementById('taskDesModel');
+
+  document.getElementById("descriptionModelHolder").append(desTemplate);
+  rootElement.setAttribute("id", desUniqueId);
+
+  rootElement.querySelectorAll("[id]").forEach(element => {
+    const desOrgId = element.getAttribute("id");
+    // console.log(desOrgId)
+    element.setAttribute("id", desOrgId + desUniqueId);
+  });
+  setTimer(desUniqueId);
+
+  const taskTimerIcon = document.getElementById("taskTimerImage" + desUniqueId);
+  const setTaskTimer = document.getElementById("setTaskTimer" + desUniqueId);
+
+  taskTimerIcon.addEventListener("mouseenter", () => {
+    setTaskTimer.classList.remove("hidden");
+  });
+
+  setTaskTimer.addEventListener("mouseleave", () => {
+    const fromHour = document.getElementById("fromHour" + desUniqueId);
+    const toHour = document.getElementById("toHour" + desUniqueId
+    );
+    const setTimerInfo = document.getElementById("timerInfo" + desUniqueId);
+    toHour.addEventListener("input", (event) => {
+    });
+    if (toHour.textContent >= fromHour.textContent) {
+      setTaskTimer.classList.add("hidden");
+      setTimerInfo.classList.add("hidden");
+    }
+    else {
+      setTimerInfo.classList.remove("hidden");
+    }
+  });
+
+  if (!desIdStore.includes(desUniqueId)) {
+    desIdStore.push(desUniqueId);
+    const newDesModel = document.getElementById(desUniqueId);
+    const taskBackdrop = document.getElementById("taskBackdrop");
+    const taskDeleteImg = document.getElementById("taskDeleteImage" + desUniqueId);
+    const fromHour = document.getElementById("fromHour" + desUniqueId);
+    const fromMin = document.getElementById("fromMin" + desUniqueId);
+    const toHour = document.getElementById("toHour" + desUniqueId);
+    const toMin = document.getElementById("toMin" + desUniqueId);
+    const setTimerInfo = document.getElementById("timerInfo" + desUniqueId);
+    taskDeleteImg.classList.add("hidden");
+    newDesModel.classList.remove("hidden");
+    taskBackdrop.classList.remove("hidden");
+    setTimerNewTask(desUniqueId);
+
+    taskBackdrop.addEventListener("click", (e) => {
+          if (toHour.textContent == "00") {
+            const setTimerInfo = document.getElementById("timerInfo" + desUniqueId);
+            setTimerInfo.classList.remove("hidden");          
+          } else {
+            newDesModel.classList.add("hidden");
+            taskBackdrop.classList.add("hidden");
+            setTimerInfo.classList.add("hidden");
+
+          }
+      e.preventDefault();
+
+      const newTaskTitle = document.getElementById("task-title-input" + desUniqueId);
+      const newTaskDes = document.getElementById("task-description-input" + desUniqueId);
+      if ((newTaskTitle.innerText).trim() != "Title" || (newTaskDes.innerText).trim() != "Starting typing...") {
+        // Check if the element is already in the DOM to avoid resetting its position
+        if (!newToDoChild.parentNode) {
+          const cloneNewtoDo = copyOtherelementChild(addTaskBtn, newToDoChild, "newToDotask" + "-" + desUniqueId);
+          newToDoChild.textContent = newTaskTitle.textContent;
+          newToDoChild.setAttribute("id", "newToDoTask" + "-" + desUniqueId);
+          todoBox.append(newToDoChild);
+          cloneNewtoDo.querySelectorAll("[id]").forEach((e) => {
+            if (e.id !== "addTaskIcon") {
+              newToDoChild.appendChild(e);
+            }
+          });
+          const newTaskHoverIcons = document.querySelector(`#newToDoTask-${desUniqueId} #taskHoverIcon`)
+          newToDoChild.addEventListener("mouseenter", (e) => {
+            e.preventDefault();
+            newTaskHoverIcons.classList.remove("hidden");
+          });
+          newToDoChild.addEventListener("mouseleave", (e) => {
+            e.preventDefault();
+            newTaskHoverIcons.classList.add("hidden");
+          });
+          newTaskHoverIcons.lastElementChild.firstChild.addEventListener("click", () => {
+            e.preventDefault();
+            newToDoChild.remove();
+            newDesModel.remove();
+            delDesId(desUniqueId);
+          });
+          initializeDragAndDrop();
+          function updateTimingRemain() {
+            let totalRemainingHours = Number(toHour.textContent) - Number(fromHour.textContent);
+            rm = totalRemainingHours * 60
+            rm -= 1;
+            newTaskHoverIcons.firstElementChild.textContent = rm;
+          };
+          updateTimingRemain();
+          setInterval(updateTimingRemain, 1000);
+        }
+      };
+    });
+
+    newToDoChild.addEventListener("click", (element) => {
+      console.log("clicked")
+      element.preventDefault();
+      const elementId = element.target.id.split("-")[1];
+      document.getElementById(elementId).classList.remove("hidden");
+      taskBackdrop.classList.remove("hidden");
+      taskDeleteImg.classList.remove("hidden");
+    });
+
+    // To delete the particular task
+    taskDeleteImg.addEventListener("click", () => {
+      newToDoChild.remove();
+      newDesModel.remove();
+      delDesId(desUniqueId);
+      taskBackdrop.classList.add("hidden");
+    })
+  };
+};
+
+// Add task button event listener
+addTaskBtn.addEventListener("click", createTask);
+
+// Menu popup event listeners
 const menuToDo = document.getElementById("menu-todo");
-const todoPopup = document.getElementById("todoMenu")
+const todoPopup = document.getElementById("todoMenu");
 menuToDo.addEventListener("mouseenter", () => {
   todoPopup.classList.toggle("hidden");
 });
-todoPopup.addEventListener('mouseleave', () => {
+todoPopup.addEventListener("mouseleave", () => {
   todoPopup.classList.toggle("hidden");
 });
+
+
+
